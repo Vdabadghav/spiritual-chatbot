be6c15b2f5d01e362cab509de54802fa13bb90c9
Vdabadghav <vdabadghav@gmail.com>
2026-03-08 14:27:23 +0530

    Added Bhagavad Gita chatbot code
    Description:
    Created embeddings and retrieval system

---
+++ pinecone_vectors_sb.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from pinecone import Pinecone, ServerlessSpec
+from sentence_transformers import SentenceTransformer
+
+PINECONE_API_KEY = "pcsk_K2TAc_AAyaJ7F1UZpeAGkShNt9MQNspNVx82xsifFkK4EhzNtiMEjQQmCbgHGyt56F2F4"
+INDEX_NAME = "srimad-bhagavatam"
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+if INDEX_NAME not in [index["name"] for index in pc.list_indexes()]:
+    pc.create_index(
+        name=INDEX_NAME,
+        dimension=384,
+        metric="cosine",
+        spec=ServerlessSpec(
+            cloud="aws",
+            region="us-east-1"
+        )
+    )
+
+index = pc.Index(INDEX_NAME)
+
+df = pd.read_csv(r"D:\work\spiritual-chatbot\data\raw\Srimadbhagwatamcsv.csv")
+
+print("Columns found:", df.columns)
+print("Total verses loaded:", len(df))
+
+df = df.fillna("")
+
+model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
+
+texts = (
+    df["Translation"].astype(str).str.strip() + " " +
+    df["Purport"].astype(str).str.strip()
+).tolist()
+
+embeddings = model.encode(texts, show_progress_bar=True)
+
+vectors = []
+
+for i, embedding in enumerate(embeddings):
+    vectors.append(
+    (
+        f"bhagavatam_{i}",
+        embedding.tolist(),
+        {
+            "canto": int(df.iloc[i]["Canto"]),
+            "chapter": int(df.iloc[i]["Chapter"]),
+            "reference": df.iloc[i]["Text"],
+            "sanskrit": df.iloc[i]["Devanagari Script"],
+            "translation": df.iloc[i]["Translation"],
+            "purport": df.iloc[i]["Purport"][:1000]
+        }
+    )
+)
+
+batch_size = 20
+
+for i in range(0, len(vectors), batch_size):
+    batch = vectors[i:i + batch_size]
+    index.upsert(vectors=batch)
+    print(f"Uploaded batch {i//batch_size + 1}")
+
+print("All Srimad Bhagavatam verses uploaded successfully!")
