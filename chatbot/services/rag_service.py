e35fd5c06dab7426197384a4e261a072a052d3b6
Vdabadghav <vdabadghav@gmail.com>
2026-03-26 16:37:36 +0530

    Update rag_service.py

--- chatbot/services/rag_service.py
+++ chatbot/services/rag_service.py
@@ -1,18 +1,12 @@
-from langchain_classic.chains.retrieval import create_retrieval_chain
-from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
-
 from chatbot.core.llm import get_gemini_llm
 from chatbot.db.vectorstore import get_retriever
-
 
 def get_rag_chain():
     llm = get_gemini_llm()
     retriever = get_retriever()
 
-    prompt = ChatPromptTemplate.from_template("""
-You are a spiritual assistant based ONLY on Bhagavad Gita and Srimad Bhagavatam.
-
+    prompt = ChatPromptTemplate.from_template("""You are a spiritual assistant based ONLY on Bhagavad Gita and Srimad Bhagavatam.
 Answer in a calm and philosophical tone.
 
 Context:
@@ -22,12 +16,24 @@ Question:
 {input}
 """)
 
-    document_chain = create_stuff_documents_chain(llm, prompt)
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
 
-    return create_retrieval_chain(retriever, document_chain)
+    def rag_pipeline(query):
+        docs = retriever.invoke(query)
+        context = format_docs(docs)
 
+        messages = prompt.invoke({
+            "context": context,
+            "input": query
+        })
+
+        response = llm.invoke(messages)
+
+        return response.content 
+
+    return rag_pipeline
 
 def get_answer(query: str):
     chain = get_rag_chain()
-    result = chain.invoke({"input": query})
-    return result["answer"]
+    return chain(query)
