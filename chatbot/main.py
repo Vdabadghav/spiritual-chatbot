956fbfae5a68fd4a227d5fa6427cf66b1e600eb1
Vdabadghav <vdabadghav@gmail.com>
2026-04-22 15:04:18 +0530

    main.py updated

--- chatbot/main.py
+++ chatbot/main.py
@@ -5,6 +5,7 @@ app = FastAPI(title="Spiritual RAG Chatbot")
 
 app.include_router(router)
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("chatbot.main:app", host="0.0.0.0", port=8001, reload=True)
+@app.get("/")
+def home():
+    return {"status": "Bot is running "}
+
